e3a9140c1db4f0d969d43ae8da2be4fc8c6fe01a
eflatmajorseven <ignas.malinauskas@stud.vgtu.lt>
2023-04-24 14:28:17 +0300

    node and react set up, installed few server dependencies

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import { Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
