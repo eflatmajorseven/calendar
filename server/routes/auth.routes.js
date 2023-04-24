2cfa569961846ef0558805e2f0ec8c30ce445f70
eflatmajorseven <ignas.malinauskas@stud.vgtu.lt>
2023-04-24 19:40:15 +0300

    sign up verification. Backlog item #PL-4

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,3 +1,4 @@
+const { verifySignUp } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 
 module.exports = function(app) {
@@ -12,6 +13,10 @@ module.exports = function(app) {
 
     app.post(
         "/api/auth/signup",
+        [
+            verifySignUp.checkDuplicateUsernameOrEmail,
+            verifySignUp.checkRolesExisted
+        ],
         controller.signup
       );
 
