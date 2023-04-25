64d62701f0e865fc69d69601773ccca9a008cc93
eflatmajorseven <ignas.malinauskas@stud.vgtu.lt>
2023-04-25 15:41:52 +0300

    login page UI and api request #Backlog item PL-5

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -12,6 +12,23 @@ const register = (username,name,lastname, email, password) => {
   });
 };
 
+const login = (username, password) => {
+    return axios
+      .post(API_URL + "signin", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+  
+        return response.data;
+      });
+  };
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    register
+    register,
+    login
 };
