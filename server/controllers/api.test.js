efa4823d88e2a2299869c68790772ce1296ce6a4
eflatmajorseven <70693562+eflatmajorseven@users.noreply.github.com>
2023-05-01 23:27:05 +0300

    Update api.test.js

--- server/controllers/api.test.js
+++ server/controllers/api.test.js
@@ -1,18 +1,18 @@
-// const api = require("./auth.controller")
-// const request = require("supertest")
-// const userToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NGZjMjAzMTFhMzQzNDNjODdlOWM0MSIsImlhdCI6MTY4Mjk0ODY2MywiZXhwIjoxNjgzMDM1MDYzfQ.Tlaz6xnx1XAanSvgvFFpZ0_k_1Bf2iP2wYedk1nnq_w"
-// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NDdlM2YxYWY2ODJmMjc3MGYyMjA2MiIsImlhdCI6MTY4Mjg4NzQ0MCwiZXhwIjoxNjgyOTczODQwfQ.jP2-JnoI6Ic_Evcm1FquXjl2FRqmSvgM-uBsQMJgbt8'
-// const authHeaders = {'x-access-token': token}
+ const api = require("./auth.controller")
+ const request = require("supertest")
+ const userToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NGZjMjAzMTFhMzQzNDNjODdlOWM0MSIsImlhdCI6MTY4Mjk0ODY2MywiZXhwIjoxNjgzMDM1MDYzfQ.Tlaz6xnx1XAanSvgvFFpZ0_k_1Bf2iP2wYedk1nnq_w"
+ const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NDdlM2YxYWY2ODJmMjc3MGYyMjA2MiIsImlhdCI6MTY4Mjg4NzQ0MCwiZXhwIjoxNjgyOTczODQwfQ.jP2-JnoI6Ic_Evcm1FquXjl2FRqmSvgM-uBsQMJgbt8'
+ const authHeaders = {'x-access-token': token}
 
-// describe ("DELETE API", () => {
-//     const calendarApi = request(`http://localhost:8080/api/auth`)
+ describe ("DELETE API", () => {
+     const calendarApi = request(`http://localhost:8080/api/auth`)
 //     const id='644f84225b40c103b4f0e3d1';
     
-//     it("GET: should return all users", async () => {
-//         await calendarApi
-//         .get("/users").set(authHeaders)
-//         .expect(200)
-//         });
+     it("GET: should return all users", async () => {
+         await calendarApi
+         .get("/users").set(authHeaders)
+         .expect(200)
+         });
 //         //fail test
 //         it ("DELETE: token test: should throw erorr without a token", async () => {
 //             const response = await calendarApi
@@ -48,4 +48,4 @@
 //         .get("/user/"+id).set(authHeaders).expect(404)
 //     });
     
-// });
+ });
